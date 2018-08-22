53c17d03dc488b3a167793b9e6242d350428e918
Unknown <railken@protonmail.ch>
2018-08-22 11:51:31 +0200

    update default route app

--- config/ore.config.php
+++ config/ore.config.php
@@ -107,7 +107,7 @@ return [
             'enabled'    => true,
             'controller' => Railken\LaraOre\Http\Controllers\App\ConfigsController::class,
             'router'     => [
-                'prefix' => '/app/configs',
+                'prefix' => '/configs',
             ],
         ],
         'admin' => [
