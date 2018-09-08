99045d16e98953dbbc5079e61c2b073dfc226c39
railken <railken@protonmail.ch>
2018-09-08 14:31:39 +0200

    update list managers

--- src/ConfigServiceProvider.php
+++ src/ConfigServiceProvider.php
@@ -22,7 +22,7 @@ class ConfigServiceProvider extends ServiceProvider
         $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
         $this->loadRoutes();
 
-        config(['ore.permission.managers' => array_merge(Config::get('ore.permission.managers', []), [
+        config(['ore.managers' => array_merge(Config::get('ore.managers', []), [
             \Railken\LaraOre\Config\ConfigManager::class,
         ])]);
 
