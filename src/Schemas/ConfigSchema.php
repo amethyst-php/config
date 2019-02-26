dd6104dfd682f320deab07aa2d6a8222e7ceecd3
railken <railken@protonmail.ch>
2019-02-26 02:48:22 +0100

    update value config

--- src/Schemas/ConfigSchema.php
+++ src/Schemas/ConfigSchema.php
@@ -19,7 +19,7 @@ class ConfigSchema extends Schema
             Attributes\TextAttribute::make('key')
                 ->setRequired(true)
                 ->setUnique(true),
-            Attributes\TextAttribute::make('value'),
+            Attributes\LongTextAttribute::make('value'),
             Attributes\EnumAttribute::make('visibility', ['private', 'public'])
                 ->setRequired(true),
             Attributes\CreatedAtAttribute::make(),
