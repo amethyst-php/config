dc5c197c5d023060ebb520d8d67b025a6b9a9d06
railken <railken@protonmail.ch>
2019-04-06 22:40:42 +0200

    add lang

---
+++ resources/lang/en/config.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+    'name' => 'Config',
+    'description' => 'Configs are a pair of key and value used to customize your application',
+    'attributes' => [
+        'key' => [
+            'label' => 'Key',
+            'description' => 'A unique name to reference the key. E.g. application.url'
+        ],
+        'value' => [
+            'label' => 'Value',
+            'description' => 'The value you wish to customize'
+        ],
+        'visibility' => [
+            'label' => 'Visibility',
+            'description' => 'Indicate wheter or not this config should be visible in public. If public this key will be accessible through API and anyone will be able to see it. If private the value will be available only within the system'
+        ]
+    ]
+]
---
+++ resources/lang/it/config.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+    'name' => 'Config',
+    'description' => 'I config sono coppie di chiavi e valori usati per customizzare la tua applicazione',
+    'attributes' => [
+        'key' => [
+            'label' => 'Chiave',
+            'description' => 'A unique name to reference the key. E.g. application.url'
+        ],
+        'value' => [
+            'label' => 'Valore',
+            'description' => 'The value you wish to customize'
+        ],
+        'visibility' => [
+            'label' => 'Visibilità',
+            'description' => 'Indicate wheter or not this config should be visible in public. If public this key will be accessible through API and anyone will be able to see it. If private the value will be available only within the system'
+        ]
+    ]
+]
